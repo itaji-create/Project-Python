9b84e9e5071c9424c19cbd5153b9affab4b99537
Leo Marinho <dev.leomarinho@gmail.com>
2023-11-27 18:16:04 -0300

    acrescentando o glossário e alterações diversas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, send_from_directory, request
+import csv
+from flask import Flask, render_template, send_from_directory, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -34,7 +35,58 @@ def salvar():
 
 @app.route('/glossario')
 def glossario():
-    return render_template("glossario.html")
+
+    glossario_de_termos = []
+
+    with open(
+            'bd_glossario.csv',
+            newline='', encoding='utf-8') as arquivo:
+        reader = csv.reader(arquivo, delimiter=';')
+        for l in reader:
+            glossario_de_termos.append(l)
+
+    return render_template('glossario.html',
+                           glossario=glossario_de_termos)
+
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('adicionar_termo.html')
+
+
+@app.route('/criar_termo', methods=['POST', ])
+def criar_termo():
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open(
+            'bd_glossario.csv', 'a',
+            newline='', encoding='utf-8') as arquivo:
+        writer = csv.writer(arquivo, delimiter=';')
+        writer.writerow([termo, definicao])
+
+    return redirect(url_for('glossario'))
+
+
+@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
+def excluir_termo(termo_id):
+
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    # Encontrar e excluir o termo com base no ID
+    for i, linha in enumerate(linhas):
+        if i == termo_id:
+            del linhas[i]
+            break
+
+    # Salvar as alterações de volta no arquivo
+    with open('bd_glossario.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
 
 @app.route('/sobre')
 def sobre():
